dea5f3646b5b285442fd11bb5e54840bfe934263
Savager17 <140598996+Savager17@users.noreply.github.com>
2024-01-18 19:52:37 +0530

    .

---
+++ Projects/6-todo-app-version-three/src/App.jsx
@@ -0,0 +1,38 @@
+import AddToDO from "./components/AddToDo";
+import AppName from "./components/AppName";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName></AppName>
+      <AddToDO onNewItem={handleNewItem}></AddToDO>
+      {todoItems.length === 0 && <WelcomeMessage />}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
+}
+
+export default App;
