c9b89a31d4bf23d8d843c1a80f035b91db33f7c8
Savager17 <140598996+Savager17@users.noreply.github.com>
2024-01-19 16:39:39 +0530

    .

--- Projects/6-todo-app-version-three/src/components/AddToDo.jsx
+++ Projects/6-todo-app-version-three/src/components/AddToDo.jsx
@@ -1,35 +1,30 @@
-import { useState } from "react";
+import { IoAdd } from "react-icons/io5";
+import { useRef } from "react";
 
 function AddToDo({ onNewItem }) {
-  const [todoName, setTodoName] = useState();
-  const [dueDate, setDueDate] = useState();
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
-
-  const handleAddButtonClicked = () => {
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setDueDate("");
-    setTodoName("");
   };
-
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <div className="row kg-row" onSubmit={handleAddButtonClicked}>
         <div className="col-6">
           <input
             type="text"
+            ref={todoNameElement}
             placeholder="Enter Todo Here"
-            value={todoName}
-            onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={handleDateChange} />
+          <input type="date" ref={dueDateElement} />
         </div>
         <div className="col-1">
           <button
@@ -37,7 +32,7 @@ function AddToDo({ onNewItem }) {
             className="btn btn-success kg-button"
             onClick={handleAddButtonClicked}
           >
-            Add
+            <IoAdd />
           </button>
         </div>
       </div>
