04e8208085453563f5d581add7fac4af440b8b20
Savager17 <140598996+Savager17@users.noreply.github.com>
2024-02-15 18:27:38 +0530

    .

--- 52_Redux/3-Counter-React-Redux-Toolkit/src/store/Index.js
+++ 52_Redux/3-Counter-React-Redux-Toolkit/src/store/Index.js
@@ -1,49 +1,13 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-
-const INITIAL_VALUE = {
-  counter: 0,
-  privacy: false
-}
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: { counterVal: 0 },
-  reducers: {
-    increment: (state, action) => {
-
-    },
-    decrement: (state, action) => {
-
-    },
-    add: (state, action) => {
-
-    },
-    subtract: (state, action) => {
-
-    }
-  }
-});
-
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  if (action.type === "INCREMENT") {
-    return { ...store, counter: store.counter + 1 };
-  } else if (action.type === "DECREMENT") {
-    return { ...store, counter: store.counter - 1, };
-  } else if (action.type === "ADD") {
-    return { ...store, counter: store.counter + Number(action.payload.num) };
-  } else if (action.type === "SUBTRACT") {
-    return { ...store, counter: store.counter - Number(action.payload.num) };
-  } else if (action.type === 'PRIVACY_TOGGLE') {
-    return { ...store, privacy: !store.privacy };
-  }
-  return store;
-}
+import counterSlice from "./counter";
+import privacySlice from "./privacy";
 
 const counterStore = configureStore({
   reducer: {
-    counter: counterSlice.reducer
+    counter: counterSlice.reducer,
+    privacy: privacySlice.reducer
   }
 });
+
 
 export default counterStore;
